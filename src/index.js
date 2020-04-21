0a9e53e153f61a4b7ec0aba93f8c6dd1b7396621
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-04-21 17:42:46 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  f = document.getElementById("create-task-form")
+
+  f.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let inputField = document.getElementById("new-task-description")
+    let value = inputField.value
+    let listField = document.getElementById("tasks")
+    listField.append(value)
+
+    });
 });
+
+
+// document.createElement('li').textContent=listField.value()
