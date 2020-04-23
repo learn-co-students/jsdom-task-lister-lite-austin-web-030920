9b7085fd3c76a72adff23020d7d30a44dbba9e45
J-Fisk87 <Jeff.Fisk512@gmail.com>
2020-04-22 20:45:39 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form#create-task-form")
+    .addEventListener("submit", event => {
+      event.preventDefault()
+      let ul = document.getElementById('tasks')
+      let li = document.createElement('li')
+      li.innerText = document.getElementById('new-task-description').value
+      ul.appendChild(li)
+    })
 });
+
+/*
+step one: event listener for submit form
+  step one-ay: .preventDefault so we do not redirect
+
+step two: take the submitted info and transform it into something usable
+  step two-ay: append it to our unordered list that looks like this:
+<div id="list">
+      <h2>My Todos</h2>
+      <ul id="tasks">
+      </ul>
+    </div>
+*/
