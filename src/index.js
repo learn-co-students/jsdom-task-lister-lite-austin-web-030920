51b070eeed264e6b64bcba9fa746962f16fe6eeb
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-04-22 20:25:43 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.getElementById("create-task-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      let ul = document.getElementById("tasks")
+
+      let li = document.createElement("li")
+
+      let newTask = document.getElementById("new-task-description")
+      
+      li.innerText = newTask.value
+
+      ul.appendChild(li)
+
+    })
 });
